fff533a2f45fadc7e8a7c7081f639cdcd0fc7db0
Michel <github@chromadigital.group>
2018-07-17 19:15:42 +0200

    http exception

---
+++ MinimalHttp/MinimalHttp/Client/HttpClientException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MinimalHttp.Client
+{
+    public class HttpClientException : Exception
+    {
+        public HttpClientException()
+        {
+        }
+
+        public HttpClientException(string message) : base(message)
+        {
+        }
+
+        public HttpClientException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected HttpClientException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
