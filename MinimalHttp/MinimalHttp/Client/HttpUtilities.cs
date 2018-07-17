06721ac0ed4d14e55320237ef840349b7ec5b05a
Michel <github@chromadigital.group>
2018-07-17 23:15:10 +0200

    made HttpUtilities helper class

---
+++ MinimalHttp/MinimalHttp/Client/HttpUtilities.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MinimalHttp.Client
+{
+    public class HttpUtilities
+    {
+        public static string UrlEncode(string text)
+        {
+            return Uri.EscapeUriString(text);
+        }
+
+        public static string UrlDecode(string text)
+        {
+            return Uri.UnescapeDataString(text);
+        }
+
+        public static string CreateContentType(string mime_type, string charset = null, string boundary = null)
+        {
+            string result = "";
+            int concenations = 0;
+
+            if (!string.IsNullOrEmpty(mime_type))
+            {
+                result += mime_type;
+
+                concenations++;
+            }
+
+            if (!string.IsNullOrEmpty(charset))
+            {
+                if (concenations > 0) result += "; ";
+
+                result += charset;
+
+                concenations++;
+            }
+
+            if (!string.IsNullOrEmpty(boundary))
+            {
+                if (concenations > 0) result += "; ";
+
+                result += boundary;
+            }
+
+            return result;
+        }
+    }
+}
