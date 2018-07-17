573fd3fec2950934ae9cbbe28048083ad2531d0c
Michel <github@chromadigital.group>
2018-07-18 01:55:21 +0200

    added test

---
+++ MinimalHttp/MinimalHttp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using MinimalHttp.Client;
+
+namespace MinimalHttp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            var response = client.Get("https://chromacheats.com");
+
+            Console.ReadLine();
+        }
+    }
+}
