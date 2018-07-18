0e4827b178397d7a6430a3f645c92e34edd2c7ca
Michel <github@chromadigital.group>
2018-07-18 20:38:31 +0200

    added more validation methods

--- MinimalHttp/MinimalHttp/Client/HttpCertificate.cs
+++ MinimalHttp/MinimalHttp/Client/HttpCertificate.cs
@@ -23,5 +23,48 @@ namespace MinimalHttp.Client
 
             return true;
         }
+
+        public bool ValidatePublicKey(byte[] key)
+        {
+            if (key == null || key.Length == 0) throw new ArgumentNullException(nameof(key));
+
+            return CompareBytes(this.GetPublicKey(), key);
+        }
+
+        private bool CompareBytes(byte[] first, byte[] second)
+        {
+            if (first == null) throw new ArgumentNullException(nameof(first));
+            if (second == null) throw new ArgumentNullException(nameof(second));
+
+            if (first.Length != second.Length) return false;
+
+            for(int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i]) return false;
+            }
+
+            return true;
+        }
+
+        public override bool Equals(X509Certificate other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+
+            return CompareBytes(this.GetRawCertData(), other.GetRawCertData());
+        }
+
+        public bool Equals(HttpCertificate other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+
+            return CompareBytes(this.GetRawCertData(), other.GetRawCertData());
+        }
+
+        public bool Equals(byte[] other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+
+            return CompareBytes(this.GetRawCertData(), other);
+        }
     }
 }
